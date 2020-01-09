ea3f0f15089e62fe34dd48ed9ef8845a766c10a8
Yonnji <yonnji@kitsune.one>
2020-01-09 21:43:03 +0300

    appx

--- jenkins.win-appx.bat
+++ jenkins.win-appx.bat
@@ -3,9 +3,19 @@ rem d:\Apps\Miniconda3_x64\Scripts\conda create --yes --prefix env
 rem d:\Apps\Miniconda3_x64\Scripts\activate ./env
 
 call "D:\Apps\Miniconda3_x64\Scripts\activate" base
+
 python appx.py
 
 call "D:\Apps\Visual Studio\IDE\VC\Auxiliary\Build\vcvarsall.bat" x64
+
 makepri createconfig /v /o /cf kitsunetsuki\priconfig.xml /dq en-US
-makepri new /v /o /cf kitsunetsuki\priconfig.xml /pr kitsunetsuki /mn kitsunetsuki/appxmanifest.xml
-makeappx pack /v /o /d kitsunetsuki /m kitsunetsuki/appxmanifest.xml /p kitsunetsuki.msix
+
+makepri new /v /o /cf kitsunetsuki\priconfig.xml ^
+    /pr kitsunetsuki ^
+    /mn kitsunetsuki/appxmanifest.xml ^
+    /mf mapping.txt
+
+makeappx pack /v /o /p kitsunetsuki.msix ^
+    /d kitsunetsuki ^
+    /m kitsunetsuki/appxmanifest.xml ^
+    /f mapping.txt
