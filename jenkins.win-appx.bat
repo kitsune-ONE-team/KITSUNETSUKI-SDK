58f87e85ac739bdb95ad89d94fc1e1c4dcbc7fa6
Yonnji <yonnji@kitsune.one>
2020-01-09 21:13:22 +0300

    appx

---
+++ jenkins.win-appx.bat
@@ -0,0 +1,5 @@
+d:\Apps\Miniconda3_x64\Scripts\conda env remove --yes --prefix env
+d:\Apps\Miniconda3_x64\Scripts\conda create --yes --prefix env
+d:\Apps\Miniconda3_x64\Scripts\activate ./env
+
+d:\Apps\Miniconda3_x64\python appx.py
