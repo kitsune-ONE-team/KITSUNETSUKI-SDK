e136c86f3ee0410be9815fefd1ac49a427ff8c8f
Yonnji <yonnji@kitsune.one>
2020-01-09 21:01:45 +0300

    appx

--- appx.py
+++ appx.py
@@ -1,11 +1,14 @@
 import json
 import os
 import zipfile
+import ssl
 
 from urllib.request import urlopen, urlretrieve
 
 
 def main():
+    ssl._create_default_https_context = ssl._create_unverified_context
+
     url = 'https://kitsune.one/k/api/release/'
     data = json.loads(urlopen(url).read().decode('utf-8'))
 
