d964ca3b743ac8ef5c1d1dbbd6845321fbaff552
Yonnji <yonnji@kitsune.one>
2020-01-09 22:09:23 +0300

    appx

--- jenkins.win-appx.bat
+++ jenkins.win-appx.bat
@@ -1,21 +1,18 @@
-rem d:\Apps\Miniconda3_x64\Scripts\conda env remove --yes --prefix env
-rem d:\Apps\Miniconda3_x64\Scripts\conda create --yes --prefix env
-rem d:\Apps\Miniconda3_x64\Scripts\activate ./env
-
 call "D:\Apps\Miniconda3_x64\Scripts\activate" base
 
 python appx.py
 
 call "D:\Apps\Visual Studio\IDE\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-makepri createconfig /v /o /cf kitsunetsuki\priconfig.xml /dq en-US
+makepri createconfig /v /o /cf priconfig.xml /dq en-US
 
-makepri new /v /o /cf kitsunetsuki\priconfig.xml ^
+makepri new /v /o /of resources.pri ^
+    /cf priconfig.xml ^
     /pr kitsunetsuki ^
-    /mn kitsunetsuki\appxmanifest.xml ^
-    /mf appx ^
-    /of kitsunetsuki\resources.pri
+    /mn appxmanifest.xml ^
+    /in appxmanifest.xml ^
+    /mf appx
 
 makeappx pack /v /o /p kitsunetsuki.msix ^
     /m kitsunetsuki\appxmanifest.xml ^
-    /f appx
+    /f resources.map.txt
