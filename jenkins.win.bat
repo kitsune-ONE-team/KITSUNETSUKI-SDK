06adfd28d2c4218fa310b0dd15f1da26ea51b1e1
Yonnji <yonnji@kitsune.one>
2020-04-03 16:18:59 +0300

    jenkins

--- jenkins.win.bat
+++ jenkins.win.bat
@@ -8,10 +8,15 @@ conda\%JOB_BASE_NAME%
 set KONDA_TOKEN=C:\Users\WIN8\.continuum\anaconda-client\tokens\https%%3A%%2F%%2Fapi.anaconda.org.token
 
 %KONDA% env remove --yes --prefix env
+
+rem if you have ssl connection problems with it
+rem copy files "libcrypto-1_1-x64.dll" and "libssl-1_1-x64.dll"
+rem from "Library/bin" to "DDLs"
 %KONDA% create --yes --prefix env
+
 %KONDA% install --prefix env conda-build anaconda-client
 
-FOR /F "tokens=*" %g IN ('env/Scripts/conda build --output %KONDA_ARGS%') do (SET KONDA_PAK=%%g)
+FOR /F "tokens=*" %%g IN ('env/Scripts/conda build --output %KONDA_ARGS%') do (SET KONDA_PAK=%%g)
 
 env\Scripts\conda build %KONDA_ARGS%
 env\Scripts\anaconda ^
