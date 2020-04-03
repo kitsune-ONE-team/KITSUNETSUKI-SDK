a05d72169e345e5af7c713c558b286116b76f0a1
Yonnji <yonnji@kitsune.one>
2020-04-03 15:54:48 +0300

    jenkins

--- jenkins.win.bat
+++ jenkins.win.bat
@@ -4,14 +4,14 @@ set KONDA_ARGS=^
 --croot croot ^
 --output-folder output ^
 --channel kitsune.one ^
-conda/%JOB_BASE_NAME%
-set KONDA_TOKEN=C:\Users\WIN8\.continuum\anaconda-client\tokens\https%3A%2F%2Fapi.anaconda.org.token
+conda\%JOB_BASE_NAME%
+set KONDA_TOKEN=C:\Users\WIN8\.continuum\anaconda-client\tokens\https%%3A%%2F%%2Fapi.anaconda.org.token
 
 %KONDA% env remove --yes --prefix env
 %KONDA% create --yes --prefix env
 %KONDA% install --prefix env conda-build anaconda-client
 
-FOR /F "tokens=*" %g IN ('env/Scripts/conda build --output %KONDA_ARGS%') do (SET KONDA_PAK=%g)
+FOR /F "tokens=*" %g IN ('env/Scripts/conda build --output %KONDA_ARGS%') do (SET KONDA_PAK=%%g)
 
 env/Scripts/conda build %KONDA_ARGS%
 env/Scripts/anaconda \
