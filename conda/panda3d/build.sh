ad52bcd2b6e60f5765a276f96ba2d2a89accbe1d
Yonnji <yonnji@kitsune.one>
2020-04-30 16:26:22 +0300

    no cmake yet

--- conda/panda3d/build.sh
+++ conda/panda3d/build.sh
@@ -17,48 +17,48 @@ python ${RECIPE_DIR}/icon.py \
     ${RECIPE_DIR}/icon_256.png \
     panda/src/x11display/icon.h
 
-# python makepanda/makepanda.py \
-#     --nothing \
-#     --use-bullet \
-#     --use-contrib \
-#     --use-deploytools \
-#     --use-direct \
-#     --use-egg \
-#     --use-freetype \
-#     --use-gl \
-#     --use-nvidiacg \
-#     --use-openal \
-#     --use-openssl \
-#     --use-pandafx \
-#     --use-pandaparticlesystem \
-#     --use-pandaphysics \
-#     --use-pandatool \
-#     --use-png \
-#     --use-pview \
-#     --use-python \
-#     --use-skel \
-#     --use-vorbis \
-#     --use-x11 \
-#     --use-zlib \
-#     --bullet-incdir ${CONDA_PREFIX}/include \
-#     --bullet-libdir ${CONDA_PREFIX}/lib \
-#     --freetype-incdir ${CONDA_PREFIX}/include \
-#     --freetype-libdir ${CONDA_PREFIX}/lib \
-#     --openssl-incdir ${CONDA_PREFIX}/include \
-#     --openssl-libdir ${CONDA_PREFIX}/lib \
-#     --png-incdir ${CONDA_PREFIX}/include \
-#     --png-libdir ${CONDA_PREFIX}/lib \
-#     --python-incdir ${CONDA_PREFIX}/include \
-#     --python-libdir ${CONDA_PREFIX}/lib \
-#     --threads=2 \
-#     --verbose
+python makepanda/makepanda.py \
+    --nothing \
+    --use-bullet \
+    --use-contrib \
+    --use-deploytools \
+    --use-direct \
+    --use-egg \
+    --use-freetype \
+    --use-gl \
+    --use-nvidiacg \
+    --use-openal \
+    --use-openssl \
+    --use-pandafx \
+    --use-pandaparticlesystem \
+    --use-pandaphysics \
+    --use-pandatool \
+    --use-png \
+    --use-pview \
+    --use-python \
+    --use-skel \
+    --use-vorbis \
+    --use-x11 \
+    --use-zlib \
+    --bullet-incdir ${CONDA_PREFIX}/include \
+    --bullet-libdir ${CONDA_PREFIX}/lib \
+    --freetype-incdir ${CONDA_PREFIX}/include \
+    --freetype-libdir ${CONDA_PREFIX}/lib \
+    --openssl-incdir ${CONDA_PREFIX}/include \
+    --openssl-libdir ${CONDA_PREFIX}/lib \
+    --png-incdir ${CONDA_PREFIX}/include \
+    --png-libdir ${CONDA_PREFIX}/lib \
+    --python-incdir ${CONDA_PREFIX}/include \
+    --python-libdir ${CONDA_PREFIX}/lib \
+    --threads=2 \
+    --verbose
 
-cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    .
-make
-make install
+# cmake \
+#     -DCMAKE_BUILD_TYPE=Release \
+#     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+#     .
+# make
+# make install
 
 cp -R ${BUILT}/bin ${PREFIX}
 cp -R ${BUILT}/etc ${PREFIX}
