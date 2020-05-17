c5bf2b5c6d11dcbd44265d97e7a72f73d4ad2fad
Yonnji <yonnji@kitsune.one>
2020-05-17 12:37:32 +0300

    runtime

--- conda/runtime/build.sh
+++ conda/runtime/build.sh
@@ -14,10 +14,10 @@ fi
 ULIBX=/usr/lib/${LARCH}-linux-gnu
 
 pip install git+https://github.com/Iotic-Labs/py-ubjson.git@v0.16.1
+pip install git+https://github.com/pytest-dev/pytest-runner.git@5.2
 pip install git+https://github.com/Moguri/panda3d-gltf.git@v0.6
 pip install git+https://github.com/httplib2/httplib2.git@v0.17.3
 pip install git+https://github.com/joestump/python-oauth2.git@v1.9
-pip install git+https://github.com/pytest-dev/pytest-runner.git@5.2
 pip install https://github.com/kitsune-ONE-team/RenderPipeline/releases/download/v${RP_VERSION}/render_pipeline-${RP_VERSION}-py3-none-any.whl
 
 mkdir -pv ${PREFIX}/kitsunetsuki-runtime/panda3d
