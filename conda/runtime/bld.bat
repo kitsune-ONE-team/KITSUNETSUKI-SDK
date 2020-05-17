486f80d33067264102475e38423d057f83d25e0e
Yonnji <yonnji@kitsune.one>
2020-05-17 12:41:35 +0300

    runtime

--- conda/runtime/bld.bat
+++ conda/runtime/bld.bat
@@ -9,7 +9,6 @@ if "%ARCH%" == "64" (
 color 0f
 
 pip install git+https://github.com/Iotic-Labs/py-ubjson.git@v0.16.1
-pip install git+https://github.com/pytest-dev/pytest-runner.git@5.2
 pip install git+https://github.com/Moguri/panda3d-gltf.git@v0.6
 pip install git+https://github.com/httplib2/httplib2.git@v0.17.3
 pip install git+https://github.com/joestump/python-oauth2.git@v1.9
@@ -120,5 +119,4 @@ pip uninstall --yes ^
     oauth2 ^
     panda3d-gltf ^
     py-ubjson ^
-    pytest-runner ^
     render-pipeline
