c3cb63740c607ccb2c288ccb2741f7d619421c07
Yonnji <yonnji@kitsune.one>
2021-03-04 21:24:24 +0300

    panda test

--- conda/panda3d/run_test.py
+++ conda/panda3d/run_test.py
@@ -5,3 +5,22 @@ import panda3d.egg
 import panda3d.fx
 import panda3d.physics
 import panda3d.skel
+
+from panda3d.core import VirtualFileSystem
+
+
+import zipfile
+
+
+with open('test.txt', 'w') as f:
+    f.write('test')
+
+
+with zipfile.ZipFile(
+        'test.zip', mode='x',
+        compression=zipfile.ZIP_DEFLATED) as f:
+    f.write('test.txt')
+
+
+vfs = VirtualFileSystem.get_global_ptr()
+assert vfs.mount('test.zip', '', 0) is True
