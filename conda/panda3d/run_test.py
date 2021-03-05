3bd95c3f93ef7ec7d75778eb40f570b36ea2cc57
Yonnji <yonnji@kitsune.one>
2021-03-05 14:38:22 +0300

    panda

--- conda/panda3d/run_test.py
+++ conda/panda3d/run_test.py
@@ -1,3 +1,6 @@
+import os
+import zipfile
+
 import panda3d.core
 import panda3d.bullet
 import panda3d.direct
@@ -6,21 +9,33 @@ import panda3d.fx
 import panda3d.physics
 import panda3d.skel
 
-from panda3d.core import VirtualFileSystem
+from panda3d.core import load_prc_file_data, VirtualFileSystem
 
 
-import zipfile
-
+if os.path.exists('test.txt'):
+    os.remove('test.txt')
 
 with open('test.txt', 'w') as f:
     f.write('test')
 
+if os.path.exists('test.zip'):
+    os.remove('test.zip')
 
 with zipfile.ZipFile(
         'test.zip', mode='x',
         compression=zipfile.ZIP_DEFLATED) as f:
     f.write('test.txt')
 
-
 vfs = VirtualFileSystem.get_global_ptr()
 assert vfs.mount('test.zip', '', 0) is True
+
+load_prc_file_data('', '''
+audio-library-name null
+window-type none
+model-path .
+''')
+
+from direct.showbase.ShowBase import ShowBase
+
+base = ShowBase()
+base.loader.load_font('Ubuntu-R.ttf')
