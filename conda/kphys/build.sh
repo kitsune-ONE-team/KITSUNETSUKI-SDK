33e31ea0189723c45cdd51727854b0ef4572a7ff
Yonnji <yonnji@kitsune.one>
2021-09-09 01:51:45 +0300

    kphys

--- conda/kphys/build.sh
+++ conda/kphys/build.sh
@@ -2,10 +2,15 @@
 
 PYTHON_VERSION=python3.9
 
+mkdir -p bld
+
+cd bld
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}/lib/${PYTHON_VERSION}/site-packages \
-    .
+    ..
 
 make
 make install
+
+cd ..
