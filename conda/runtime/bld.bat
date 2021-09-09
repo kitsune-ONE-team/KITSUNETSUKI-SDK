6c071340567c996100058ea0ba9a545184f6f803
Yonnji <yonnji@kitsune.one>
2021-09-09 23:22:11 +0300

    runtime

--- conda/runtime/bld.bat
+++ conda/runtime/bld.bat
@@ -10,7 +10,7 @@ set RP_VERSION=2.0.1
 
 pip install git+https://github.com/Moguri/panda3d-gltf.git@v0.13
 pip install git+https://github.com/Iotic-Labs/py-ubjson.git@v0.16.1
-pip install git+https://github.com/httplib2/httplib2.git@v0.17.3
+pip install git+https://github.com/httplib2/httplib2.git@v0.19.1
 pip install git+https://github.com/joestump/python-oauth2.git@v1.9
 pip install https://github.com/kitsune-ONE-team/RenderPipeline/releases/download/v%RP_VERSION%/render_pipeline-%RP_VERSION%-py3-none-any.whl
 
@@ -93,7 +93,7 @@ D:\Apps\7-Zip\7z a ^
     %CONDA_PREFIX%\Lib\site-packages\direct ^
     %CONDA_PREFIX%\Lib\site-packages\gltf ^
     %CONDA_PREFIX%\Lib\site-packages\httplib2 ^
-    %CONDA_PREFIX%\Lib\site-packages\httplib2-0.19.3.dist-info ^
+    %CONDA_PREFIX%\Lib\site-packages\httplib2-0.19.1.dist-info ^
     %CONDA_PREFIX%\Lib\site-packages\idna ^
     %CONDA_PREFIX%\Lib\site-packages\jsonschema ^
     %CONDA_PREFIX%\Lib\site-packages\jsonschema-3.2.0.dist-info ^
