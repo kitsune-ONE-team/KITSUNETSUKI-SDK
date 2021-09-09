0a85f6e98d7856d05a61d66dc4b1e225c0fa3040
Yonnji <yonnji@kitsune.one>
2021-09-09 12:56:00 +0300

    blender

--- conda/blender/bld.bat
+++ conda/blender/bld.bat
@@ -19,21 +19,20 @@ copy /V /Y %CONDA_PREFIX%\python3.dll lib\%THIRDPARTY%\python\39\bin
 robocopy /E %RECIPE_DIR%\boost lib\%THIRDPARTY%\boost
 
 if not exist blender\bld (
+   mkdir blender\bld
+
    cd blender
-   mkdir bld
    patch -p1 -i %RECIPE_DIR%\platform.patch
    patch -p1 -i %RECIPE_DIR%\tbb.patch
    cd ..
 )
 
-cd blender
+robocopy /E blender_extras\release\scripts\addons         blender\release\scripts\addons
+robocopy /E blender_extras\release\scripts\addons_contrib blender\release\scripts\addons_contrib
+robocopy /E blender_extras\release\datafiles\locale       blender\release\datafiles\locale
+robocopy /E blender_extras\source\tools                   blender\source\tools
 
-robocopy /E ..\blender_extras\release\scripts\addons release\scripts\addons
-robocopy /E ..\blender_extras\release\scripts\addons_contrib release\scripts\addons_contrib
-robocopy /E ..\blender_extras\release\datafiles\locale release\datafiles\locale
-robocopy /E ..\blender_extras\source\tools source\tools
-
-cd bld
+cd blender\bld
 cmake -G "NMake Makefiles" ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX=%PREFIX%\Lib\site-packages ^
@@ -88,6 +87,6 @@ copy /V /Y bin\bpy.pyd %PREFIX%\Lib\site-packages
 robocopy /E %PREFIX%\Lib\site-packages\%PKG_VERSION% %PREFIX%\%PKG_VERSION%
 rd /s /q %PREFIX%\Lib\site-packages\%PKG_VERSION%
 
-cd ..
+cd ..\..
 
 copy /V /Y lib\%THIRDPARTY%\gmp\lib\*.dll %PREFIX%\DLLs
