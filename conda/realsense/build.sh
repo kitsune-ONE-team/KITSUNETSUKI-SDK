2b283c41645ec2fcdc539455972e69ab73f5af56
Yonnji <yonnji@kitsune.one>
2021-09-28 00:06:35 +0300

    realsense initial

---
+++ conda/realsense/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+mkdir -p bld
+cd bld
+cmake \
+    -DBUILD_EXAMPLES=OFF \
+    -DBUILD_GLSL_EXTENSIONS=OFF \
+    -DBUILD_LEGACY_LIVE_TEST=OFF \
+    -DBUILD_NETWORK_DEVICE=OFF \
+    -DBUILD_PYTHON_BINDINGS=ON \
+    -DBUILD_UNIT_TESTS=OFF \
+    -DBUILD_WITH_TM2=OFF \
+    -DCMAKE_BUILD_TYPE=Release \
+    ..
+
+make
+make install
+
+cd ..
