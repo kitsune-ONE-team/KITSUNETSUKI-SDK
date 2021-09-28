ca2969a7a11333647a45e02b4fe6ec5a833838a2
Yonnji <yonnji@kitsune.one>
2021-09-28 11:04:16 +0300

    realsense

--- version_up.sh
+++ version_up.sh
@@ -4,7 +4,7 @@ VERSION=$(date +%s)
 
 TARGETS=${1};
 if [ "${1}" = "all" ]; then
-    TARGETS="blender bullet panda3d kphys runtime";
+    TARGETS="blender bullet panda3d kphys runtime realsense";
 fi
 if [ "${1}" = "panda" ]; then
     TARGETS="panda3d kphys runtime";
