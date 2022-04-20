8bc8d2c95dcfa077808b11e3d0d2d66d5e3db1ab
Yonnji <yonnji@kitsune.one>
2022-04-20 18:40:48 +0300

    sdl

--- conda/sdl/build.sh
+++ conda/sdl/build.sh
@@ -5,6 +5,11 @@ cd bld
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DSDL_LIBC=OFF \
+    -DSDL_OSS=OFF \
+    -DSDL_SHARED=ON \
+    -DSDL_STATIC=ON \
+    -DSDL_STATIC_PIC=ON \
     ..
 
 make
