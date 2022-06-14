006a1c41d3eec713b00703b1747fe739954dc3bb
Yonnji <yonnji@kitsune.one>
2022-06-15 00:51:29 +0300

    containers

--- container/sdk_main/podman_run.sh
+++ container/sdk_main/podman_run.sh
@@ -3,10 +3,7 @@
 podman run \
        --tty --interactive \
        --name sdk_main \
-       --volume /home/${USERNAME}/Dev/kitsunetsuki:/home/${USERNAME}/Dev/kitsunetsuki:z \
-       --volume /home/${USERNAME}/Dev/vtuber_webcam:/home/${USERNAME}/Dev/vtuber_webcam:z \
-       --volume /home/${USERNAME}/Dev/yonnji:/home/${USERNAME}/Dev/yonnji:z \
-       --volume /home/${USERNAME}/Dev/kdata:/home/${USERNAME}/Dev/kdata:z \
+       --volume /home/${USERNAME}/Dev/panda:/home/${USERNAME}/Dev/panda:z \
        --env HOME=/home/${USERNAME} \
        docker.io/yonnji/sdk:main \
        bash --init-file /etc/profile
