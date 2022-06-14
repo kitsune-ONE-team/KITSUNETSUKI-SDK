b070c6a2cef3bff571462724b409c21dee3a07f1
Yonnji <yonnji@kitsune.one>
2022-06-14 23:16:22 +0300

    python version

--- conda/kphys/build.sh
+++ conda/kphys/build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-PYTHON_VERSION=python3.9
+PYTHON_VERSION=3.10
 
 mkdir -p bld
 
 cd bld
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX}/lib/${PYTHON_VERSION}/site-packages \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX}/lib/python${PYTHON_VERSION}/site-packages \
     ..
 
 make
