e9107d47a26b39cc0af3e734898018681221879d
Yonnji <yonnji@kitsune.one>
2022-06-14 14:52:44 +0300

    blender

--- container/sdk_base/podman.sh
+++ container/sdk_base/podman.sh
@@ -8,6 +8,7 @@ podman run \
     --volume $(realpath ${DIR}/conda):/home/${USERNAME}/.conda:z \
     --volume /home/${USERNAME}/Dev/kitsunetsuki:/home/${USERNAME}/Dev/kitsunetsuki:z \
     --volume /home/${USERNAME}/Dev/vtuber_webcam:/home/${USERNAME}/Dev/vtuber_webcam:z \
+    --volume /home/${USERNAME}/Dev/yonnji:/home/${USERNAME}/Dev/yonnji:z \
     --volume /home/${USERNAME}/Dev/kdata:/home/${USERNAME}/Dev/kdata:z \
     --env HOME=/home/${USERNAME} \
     docker.io/yonnji/sdk:base \
