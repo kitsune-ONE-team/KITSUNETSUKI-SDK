33add83b4c957015353cf8b10d559963c032cd6f
Yonnji <yonnji@kitsune.one>
2022-06-15 16:03:26 +0300

    blender win

--- conda/blender/bld.bat
+++ conda/blender/bld.bat
@@ -95,4 +95,4 @@ cd ..\..
 
 copy /V /Y lib\%THIRDPARTY%\ffmpeg\lib\*.dll %PREFIX%\DLLs
 copy /V /Y lib\%THIRDPARTY%\gmp\lib\*.dll %PREFIX%\DLLs
-copy /V /Y lib\%THIRDPARTY%\gmp\sndfile\*.dll %PREFIX%\DLLs
+copy /V /Y lib\%THIRDPARTY%\sndfile\lib\*.dll %PREFIX%\DLLs
