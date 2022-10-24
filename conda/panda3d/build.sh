3a20e24d63bd0f150481e315e371ed096a0151cf
Yonnji <yonnji@kitsune.one>
2022-10-24 20:12:56 +0300

    panda

--- conda/panda3d/build.sh
+++ conda/panda3d/build.sh
@@ -16,46 +16,62 @@ python ${RECIPE_DIR}/icon.py \
     ${RECIPE_DIR}/icon_256.png \
     panda/src/x11display/icon.h
 
-python makepanda/makepanda.py \
-    --bullet-incdir ${CONDA_PREFIX}/include \
-    --bullet-libdir ${CONDA_PREFIX}/lib \
-    --freetype-incdir ${CONDA_PREFIX}/include \
-    --freetype-libdir ${CONDA_PREFIX}/lib \
-    --openssl-incdir ${CONDA_PREFIX}/include \
-    --openssl-libdir ${CONDA_PREFIX}/lib \
-    --png-incdir ${CONDA_PREFIX}/include \
-    --png-libdir ${CONDA_PREFIX}/lib \
-    --python-incdir ${CONDA_PREFIX}/include \
-    --python-libdir ${CONDA_PREFIX}/lib \
-    --vorbis-incdir ${CONDA_PREFIX}/include \
-    --vorbis-libdir ${CONDA_PREFIX}/lib \
-    --zlib-incdir ${CONDA_PREFIX}/include \
-    --zlib-libdir ${CONDA_PREFIX}/lib \
-    --nothing \
-    --outputdir ${BUILT} \
-    --threads=2 \
-    --use-bullet \
-    --use-contrib \
-    --use-deploytools \
-    --use-direct \
-    --use-egg \
-    --use-freetype \
-    --use-gl \
-    --use-harfbuzz \
-    --use-openal \
-    --use-openssl \
-    --use-pandafx \
-    --use-pandaparticlesystem \
-    --use-pandaphysics \
-    --use-pandatool \
-    --use-png \
-    --use-pview \
-    --use-python \
-    --use-skel \
-    --use-vorbis \
-    --use-x11 \
-    --use-zlib \
-    --verbose
+# python makepanda/makepanda.py \
+#     --bullet-incdir ${CONDA_PREFIX}/include \
+#     --bullet-libdir ${CONDA_PREFIX}/lib \
+#     --freetype-incdir ${CONDA_PREFIX}/include \
+#     --freetype-libdir ${CONDA_PREFIX}/lib \
+#     --openssl-incdir ${CONDA_PREFIX}/include \
+#     --openssl-libdir ${CONDA_PREFIX}/lib \
+#     --png-incdir ${CONDA_PREFIX}/include \
+#     --png-libdir ${CONDA_PREFIX}/lib \
+#     --python-incdir ${CONDA_PREFIX}/include \
+#     --python-libdir ${CONDA_PREFIX}/lib \
+#     --vorbis-incdir ${CONDA_PREFIX}/include \
+#     --vorbis-libdir ${CONDA_PREFIX}/lib \
+#     --zlib-incdir ${CONDA_PREFIX}/include \
+#     --zlib-libdir ${CONDA_PREFIX}/lib \
+#     --nothing \
+#     --outputdir ${BUILT} \
+#     --threads=2 \
+#     --use-bullet \
+#     --use-contrib \
+#     --use-deploytools \
+#     --use-direct \
+#     --use-egg \
+#     --use-freetype \
+#     --use-gl \
+#     --use-harfbuzz \
+#     --use-openal \
+#     --use-openssl \
+#     --use-pandafx \
+#     --use-pandaparticlesystem \
+#     --use-pandaphysics \
+#     --use-pandatool \
+#     --use-png \
+#     --use-pview \
+#     --use-python \
+#     --use-skel \
+#     --use-vorbis \
+#     --use-x11 \
+#     --use-zlib \
+#     --verbose
+
+mkdir -p ${BUILT}
+cd ${BUILT}
+cmake \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_DTOOL=ON \
+    -DBUILD_PANDA=ON \
+    -DBUILD_DIRECT=ON \
+    -DBUILD_PANDATOOL=ON \
+    -DBUILD_CONTRIB=ON \
+    -DBUILD_MODELS=ON \
+    ..
+
+make
+cd ..
 
 python makepanda/makewheel.py \
     --outputdir ${BUILT} \
