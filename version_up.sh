e07350af9533411cdd74e07f082021e4582f6b8d
Yonnji <yonnji@kitsune.one>
2023-07-07 15:54:35 +0300

    version up

--- version_up.sh
+++ version_up.sh
@@ -4,13 +4,13 @@ VERSION=$(date +%s)
 
 TARGETS=${1};
 if [ "${1}" = "all" ]; then
-    TARGETS="blender bullet panda3d panda3d-kphys realsense runtime sdl";
+    TARGETS="blender bullet panda3d panda3d-kphys panda3d-krender runtime realsense sdl";
 fi
 if [ "${1}" = "panda" ]; then
-    TARGETS="panda3d panda3d-kphys runtime";
+    TARGETS="panda3d panda3d-kphys panda3d-krender runtime";
 fi
 if [ "${1}" = "sdk" ]; then
-    TARGETS="bullet panda3d panda3d-kphys runtime";
+    TARGETS="bullet panda3d panda3d-kphys panda3d-krender runtime";
 fi
 
 for i in ${TARGETS}; do
