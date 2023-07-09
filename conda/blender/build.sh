1756a2fad9da4797af3c8f0e8791cce4a4407ab3
Yonnji <yonnji@kitsune.one>
2023-07-09 11:06:31 +0300

    blender

--- conda/blender/build.sh
+++ conda/blender/build.sh
@@ -12,10 +12,7 @@ CMAKE_ARGS=
 cd blender/bld
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_FIND_ROOT_PATH="${PREFIX};${PREFIX}/x86_64-conda-linux-gnu/sysroot;${THIRDPARTY};/usr;/usr/lib/x86_64-linux-gnu" \
-    -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY \
-    -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
-    -DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER \
+    -DCMAKE_FIND_ROOT_PATH="${PREFIX};${PREFIX}/x86_64-conda-linux-gnu/sysroot;/usr;/usr/lib/x86_64-linux-gnu" \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_LINKER=${PREFIX}/bin/x86_64-conda-linux-gnu-ld \
