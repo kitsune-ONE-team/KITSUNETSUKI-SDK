a7ead2d6f01404d47e6e3c7c358d45d477613de8
Yonnji <yonnji@kitsune.one>
2023-07-09 10:54:11 +0300

    blender

--- conda/blender/build.sh
+++ conda/blender/build.sh
@@ -7,10 +7,19 @@ rm -Rf ${THIRDPARTY}/python
 
 mkdir -p blender/bld
 
+CMAKE_ARGS=
+
 cd blender/bld
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_FIND_ROOT_PATH=${PREFIX};${PREFIX}/x86_64-conda-linux-gnu/sysroot;/usr \
+    -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY \
+    -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
+    -DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER \
+    -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_LINKER=${PREFIX}/bin/x86_64-conda-linux-gnu-ld \
+    -DCMAKE_STRIP=${PREFIX}/bin/x86_64-conda-linux-gnu-strip \
     -DPYTHON_INCLUDE_DIR=${CONDA_PREFIX}/include/python${PYTHON_VERSION} \
     -DPYTHON_LIBRARY=${CONDA_PREFIX}/lib/libpython${PYTHON_VERSION}.so \
     -DPYTHON_VERSION=${PYTHON_VERSION} \
