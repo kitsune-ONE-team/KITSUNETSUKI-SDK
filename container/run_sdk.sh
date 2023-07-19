09c0e5e10a595e80e19dccb6a67580531635d2a0
Yonnji <yonnji@kitsune.one>
2023-07-19 10:16:21 +0300

    container

--- container/run_sdk.sh
+++ container/run_sdk.sh
@@ -7,11 +7,11 @@ echo -en "\033]0;sdk\a"
 podman run \
     --tty --interactive \
     --name sdk \
+    --volume $(realpath ${DIR}/jenkins):/root/jenkins:z \
     --volume /home/${USER}/Dev/Panda3D:/home/${USER}/Dev/Panda3D:z \
     --volume $(realpath ${DIR}/itch):/home/${USER}/.config/itch:z \
-    --volume $(realpath ${DIR}/conda/envs):/opt/miniconda3/envs:z \
     --env HOME=/home/${USER} \
     docker.io/yonnji/sdk \
-    bash --init-file /etc/profile
+    bash
 
 podman rm sdk
--- container/run_sdk_builder.sh
+++ container/run_sdk_builder.sh
@@ -8,8 +8,6 @@ podman run \
        --tty --interactive \
        --name sdk_builder \
        --volume $(realpath ${DIR}/jenkins):/root/jenkins:z \
-       --volume $(realpath ${DIR}/jenkins_jobs):/root/.config/jenkins_jobs:z \
-       --volume $(realpath ${DIR}/binstar):/root/.config/binstar:z \
        --publish 0.0.0.0:8080:8080 \
        docker.io/yonnji/sdk \
        run-jenkins
